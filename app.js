55a1bcbec90086b1eee4501f369cc9b22a79af11
styp3r <stypertypes@gmail.com>
2021-07-31 17:33:51 +0530

    add glitch effect on main headers using math functions and ascii characters

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
-$(".guideAnimation").css("width", width);
-$(".guideAnimation").css("height", height);
+console.log(height);
+$(".landingSection").css("height", height);
+$(".landingSection").css("width", width);
 
-$( ".bl" ).click(function() {
-  $( ".guideAnimation" ).addClass("blur");
+var glitch = ["░", "▒", "▓", "█", ">", "<","/"];
+
+var timeInterval = setInterval(heroTitle, 100);
+
+  function heroTitle(){
+    $(".headerIntro").text(showGlitch());//showGlitch();
+    $(".myName").text(showGlitch());
+    $(".whatIDo").text(showGlitch());
+    $(".iAm").text(showGlitch());
+
+    setTimeout(function(){
+      clearInterval(timeInterval);
+        $(".headerIntro").text("My name is");
+        $(".myName").text("Navjyot Jaiswal.");
+        $(".whatIDo").text("I build for the web.");
+        $(".iAm").html("I am a <span class = 'fewd'>Front-End Web Developer</span> based in Bangalore, India specializing in next-generation web design and occasionally create exceptional interactive user experiences.");
+    }, 3500);
+}
+
+function showGlitch(){
+  var c = glitch[Math.floor(Math.random()*6) + 1] +
+  glitch[Math.floor(Math.random()*6) + 1]+glitch[Math.floor(Math.random()*6) + 1] +
+  glitch[Math.floor(Math.random()*6) + 1]+glitch[Math.floor(Math.random()*6) + 1] +
+  glitch[Math.floor(Math.random()*6) + 1]+glitch[Math.floor(Math.random()*6) + 1] +
+  glitch[Math.floor(Math.random()*6) + 1]+glitch[Math.floor(Math.random()*6) + 1] +
+  glitch[Math.floor(Math.random()*6) + 1]+glitch[Math.floor(Math.random()*6) + 1] +
+  glitch[Math.floor(Math.random()*6) + 1];
+  return c;
+}
+
+setTimeout(function() {
+  $( ".guideAnimationTest" ).addClass("blur");
+
   setTimeout(function(){
-    $( ".guideAnimation" ).addClass("blurStatic");
- }, 3500);
-});
+    $( ".guideAnimationTest" ).addClass("blurStatic");
+ }, 3000);
+
+}, 6500);
