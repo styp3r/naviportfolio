713e7360c50751117fd2a97afc670807bf1231f2
styp3r <stypertypes@gmail.com>
2021-08-02 08:25:50 +0530

    add contact button animation and change-colour-on-hover effect

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $(".landingSection").css("width", width);
 
 var glitch = ["░", "▒", "▓", "█", ">", "<","/"];
 
-var timeInterval = setInterval(heroTitle, 100);
+var timeInterval = setInterval(heroTitle, 70);
 
   function heroTitle(){
     $(".headerIntro").text(showGlitch());//showGlitch();
@@ -20,7 +20,7 @@ var timeInterval = setInterval(heroTitle, 100);
         $(".myName").text("Navjyot Jaiswal.");
         $(".whatIDo").text("I build for the web.");
         $(".iAm").html("I am a <span class = 'fewd'>Front-End Web Developer</span> based in Bangalore, India specializing in next-generation web design and occasionally create exceptional interactive user experiences.");
-    }, 3500);
+    }, 2000);
 }
 
 function showGlitch(){
@@ -42,3 +42,45 @@ setTimeout(function() {
  }, 3000);
 
 }, 6500);
+
+setTimeout(function(){
+  $( ".navBar" ).animate({
+    opacity: 1
+  }, 1000, function() {
+    // Animation complete.
+  });
+}, 2000);
+
+setTimeout(function(){
+  $( ".encircle" ).animate({
+    opacity: 1
+  }, 1000, function() {
+    // Animation complete.
+  });
+}, 3000);
+
+$( ".line" ).animate({
+  width: 60,
+  marginLeft: 100
+}, 3000, function() {
+  // Animation complete.
+  $(".line").animate({
+    marginLeft: 0
+  }, 5000);
+});
+
+setTimeout(function(){
+  $( ".social" ).animate({
+    opacity: 1
+  }, 2000, function() {
+    // Animation complete.
+  });
+}, 3000);
+
+$(".contactButton").hover(()=>{
+  $(".rotate").css("transitionDuration", "0.5s");
+  $(".rotate").css("backgroundColor", "#9933ff");
+}, ()=>{
+  $(".rotate").css("transitionDuration", "0.5s");
+  $(".rotate").css("backgroundColor", "#ffffff");
+});
