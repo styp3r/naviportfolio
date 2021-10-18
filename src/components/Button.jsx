faa8e1bc1b266b8a7eb67f8da80dcec2dfcbf51d
Navjyot Jaiswal <stypertypes@gmail.com>
2021-10-19 02:35:23 +0530

    Remove unecessary code

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,54 +1,7 @@
 import React from "react";
 
-export default function Button() {
-    let interval;
-    let flag = 0;
+export default function Button(){ 
+  
 
-  return (
-    <div className="btn">
-        <div
-          id="button"
-          onMouseOver={() => {
-            document.getElementById("button").style.width = "11vmin";
-            document.getElementById("button").style.transitionDuration = "0.2s";
-            interval = setTimeout(()=>{
-                flag = 1;
-                document.getElementById("button").style.width = "8vmin";
-                document.getElementById("button").style.height = "8vmin";
-                document.getElementById("button").style.borderRadius = "100%";
-                document.getElementById("button").style.transitionDuration = "0.2s";
-            }, 700);
-          }}
-          onMouseOut={() => {
-                clearInterval(interval);
-                document.getElementById("button").style.width = "10vmin";
-                document.getElementById("button").style.height = "2vmin";
-                document.getElementById("button").style.borderRadius = "2vmin";
-                document.getElementById("button").style.transitionDuration = "0.2s";
-          }}
-          onMouseUp={() => {
-                document.getElementById("button").style.backgroundColor = "#ffffff";
-                document.getElementById("button").style.boxShadow = "none";
-                document.getElementById("button").style.transitionDuration = "0.2s";
-                document.getElementById("button").style.width = "10vmin";
-                document.getElementById("button").style.height = "2vmin";
-                document.getElementById("button").style.borderRadius = "2vmin";
-                flag = 0;
-          }}
-          onMouseDown={() => {
-            clearInterval(interval);
-              if(flag === 1){
-                document.getElementById("button").style.width = "9vmin";
-                document.getElementById("button").style.height = "9vmin";
-                document.getElementById("button").style.borderRadius = "100%";
-                document.getElementById("button").style.transitionDuration = "0.2s";
-              } else {
-                document.getElementById("button").style.backgroundColor = "#f2f2f2";
-                document.getElementById("button").style.width = "8vmin";
-                document.getElementById("button").style.transitionDuration = "0.2s";
-              }
-          }}
-        ></div>
-      </div>
-  );
+   return <div id = "button" ></div>;
 }
