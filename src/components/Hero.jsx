14e601d9c1181aa4ca87d19943156729781137aa
Navjyot Jaiswal <stypertypes@gmail.com>
2021-10-23 23:56:04 +0530

    Clear all code to add 3d Model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import HelloHeader from '../images/helloBlackFinal.jpg';
 
 function Hero(){
-
-    let width = window.innerWidth;
-    let height = window.innerHeight;
     return (
-        <div className = "heroSection" style = {{width: width, height: height}}>
-        <div className = "heroHeader">
-            <img className = "hello" src = {HelloHeader} width = "500" height="300"/>
-            <div className = "iAm"><span className = "topTextIAm">I am </span>Navjyot Jaiswal</div>
-        </div>
-        <p className = "whatAmI">Interactive Web Developer</p>
-        </div>
+        <h1>Hello</h1>
     );
 }
 
