dfe98c2bc9c10094ce313e400d00377568bc41ea
Navjyot Jaiswal <stypertypes@gmail.com>
2021-10-30 08:47:03 +0530

    Creates two wormholes for the big monster's hand to go through and engage with the GlowBall

---
+++ src/components/WormHole.jsx
@@ -0,0 +1,12 @@
+function WormHole(){
+    return (
+        <div id = "wormHoleContainer">
+            <div id = "wormHole">
+                <div id = "wormHoleEntry"></div>
+                <div id = "wormHoleExit"></div>
+            </div>
+        </div>
+    );
+}
+
+export default WormHole; 
