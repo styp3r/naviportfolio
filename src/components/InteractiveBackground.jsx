99aa98975275aec98ee9062472e537ece20c6a13
Navjyot Jaiswal <stypertypes@gmail.com>
2021-10-30 05:53:29 +0530

    Creates new component which will have the interactive elements along with their animations in CSS

---
+++ src/components/InteractiveBackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InteractiveBackground(){
+    return (
+        <div className = "interactiveContainer">
+            
+        </div>
+    );
+}
+
+export default InteractiveBackground;
