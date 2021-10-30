cbd8ad5661874d6eec0a5e77790936ff8b8c5447
Navjyot Jaiswal <stypertypes@gmail.com>
2021-10-30 08:45:04 +0530

    Creates BigMonster component which contains a big monster character for user interaction

---
+++ src/components/BigMonster.jsx
@@ -0,0 +1,28 @@
+function BigMonster(){
+    return (
+        <div id = "bigMonsterContainer">
+        <div id ="bigMonsterReflection"></div>
+        <div id = "bigMonsterBodyReflection"></div>
+        <div id = "bigMonster">
+            <div id = "bigMonsterLeftEye">
+                <div id = "leftPupil">
+                <div id = "leftEyeSpark"></div>
+                </div>
+            </div>
+            <div id = "bigMonsterRightEye">
+                <div id = "rightPupil">
+                    <div id = "rightEyeSpark"></div>
+                </div>
+            </div>
+            <div id = "bigMonsterMouth"></div>
+        </div>
+        <div id = "bigMonsterBody"></div>
+        <div id = "bigMonsterHand">
+            <div id = "bigMonsterUpperArm"></div>
+            <div id = "bigMonsterLowerArm"></div>
+        </div>
+        </div>
+    );
+}
+
+export default BigMonster;
