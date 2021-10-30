416e54529822cc6dcdbae742148854586e10344f
Navjyot Jaiswal <stypertypes@gmail.com>
2021-10-30 08:42:43 +0530

    Creates three components - GlowBall, BigMonster and WormHole, which are going to be animated using CSS.

--- src/components/InteractiveBackground.jsx
+++ src/components/InteractiveBackground.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import GlowBall from './GlowBall';
+import BigMonster from './BigMonster';
+import WormHole from './WormHole';
 
 function InteractiveBackground(){
+
+    let sectionWidth = window.innerWidth;
+    let sectionHeight = window.innerHeight;
+
     return (
-        <div className = "interactiveContainer">
-            
+        <div className = "interactiveContainer" style = {{width: sectionWidth, height: sectionHeight }}>
+            <GlowBall />
+            <BigMonster />
+            <WormHole />
         </div>
     );
 }
