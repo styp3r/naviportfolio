a5eb5a1ba964a4d4dea083908049cb19a4dde232
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-01 05:01:16 +0530

    Creates Octopus creature component because it looks cool

---
+++ src/components/Octopus.jsx
@@ -0,0 +1,20 @@
+function Octopus(){
+    return (
+        <div className = "octopusContainer">
+            <div id = "octopusHead">
+                <div id = "octopusEye">
+                    <div id = "octopusPupil"></div>
+                </div>
+            </div>
+
+            <div id = "octopusMouth"></div>
+
+            <div id = "octopusBody"></div>
+            <div id = "octopusLeg1"></div>
+            <div id = "octopusLeg2"></div>
+            <div id = "octopusLeg3"></div>
+        </div>
+    );
+}
+
+export default Octopus;
