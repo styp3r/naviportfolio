2c30f4e90dc8cd629b0cfd79f144baaf6f5f03f6
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-02 10:27:57 +0530

    Changes backgroundColor of interactiveContainer, heroText1 and and sets duration time & delays according to octopus swoop and eat.

--- src/components/GlowBall.jsx
+++ src/components/GlowBall.jsx
@@ -3,10 +3,24 @@ function GlowBall(){
     function toggleDark(){
         document.getElementById("glowBall").className = "glowBallDark";
         document.getElementById("octopusHead").className = "octopusHeadSlurp";
+        document.getElementById("heroText1").style.color = "#bb99ff";
+        document.getElementById("heroText1").style.transitionDelay = "2.5s";
+        document.getElementById("interactiveContainer").style.backgroundColor = "#151e1e";
+        document.getElementById("interactiveContainer").style.transitionDelay = "2.5s";
+        document.getElementById("interactiveContainer").style.transitionDuration = "0.7s";
+
 
         setTimeout(() => {
             document.getElementById("glowBall").className = "";
+            document.getElementById("glowBall").style.width = "5vmin";
+            document.getElementById("glowBall").style.height = "5vmin";
+            document.getElementById("glowBall").style.transitionDuration = "1s";
+            document.getElementById("heroText1").style.color = "#000000";
+            document.getElementById("heroText1").style.transitionDelay = "0s";
             document.getElementById("octopusHead").className = "hoverOctopusHead";
+            document.getElementById("interactiveContainer").style.backgroundColor = "#bb99ff";
+            document.getElementById("interactiveContainer").style.transitionDelay = "0s";
+            document.getElementById("interactiveContainer").style.transitionDuration = "1s";
         }, 6000);
     }
 
