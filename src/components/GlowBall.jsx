85c8e6b014eb72cf854c2732f0655be0777d9107
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-04 01:21:36 +0530

    Adds classname using DOM to ocotpus body, mouth, leg1 leg2, leg3 to enable swoop action.

--- src/components/GlowBall.jsx
+++ src/components/GlowBall.jsx
@@ -1,26 +1,39 @@
 function GlowBall(){
 
+    let glowBall = document.getElementById("glowBall");
+    let interactiveContainer = document.getElementById("interactiveContainer");
+
     function toggleDark(){
-        document.getElementById("glowBall").className = "glowBallDark";
+        glowBall.className = "glowBallDark";
         document.getElementById("octopusHead").className = "octopusHeadSlurp";
+        document.getElementById("octopusBody").className = "octopusBodySlurp";
+        document.getElementById("octopusMouth").className = "octopusMouthSlurp";
+        document.getElementById("octopusLeg1").className = "octopusLeg1Slurp";
+        document.getElementById("octopusLeg2").className = "octopusLeg2Slurp";
+        document.getElementById("octopusLeg3").className = "octopusLeg3Slurp";
         document.getElementById("heroText1").style.color = "#bb99ff";
         document.getElementById("heroText1").style.transitionDelay = "2.5s";
-        document.getElementById("interactiveContainer").style.backgroundColor = "#151e1e";
-        document.getElementById("interactiveContainer").style.transitionDelay = "2.5s";
-        document.getElementById("interactiveContainer").style.transitionDuration = "0.7s";
+        interactiveContainer.style.backgroundColor = "#151e1e";
+        interactiveContainer.style.transitionDelay = "2.5s";
+        interactiveContainer.style.transitionDuration = "0.7s";
 
 
         setTimeout(() => {
-            document.getElementById("glowBall").className = "";
-            document.getElementById("glowBall").style.width = "5vmin";
-            document.getElementById("glowBall").style.height = "5vmin";
-            document.getElementById("glowBall").style.transitionDuration = "1s";
+            glowBall.className = "";
+            glowBall.style.width = "5vmin";
+            glowBall.style.height = "5vmin";
+            glowBall.style.transitionDuration = "1s";
             document.getElementById("heroText1").style.color = "#000000";
             document.getElementById("heroText1").style.transitionDelay = "0s";
             document.getElementById("octopusHead").className = "hoverOctopusHead";
-            document.getElementById("interactiveContainer").style.backgroundColor = "#bb99ff";
-            document.getElementById("interactiveContainer").style.transitionDelay = "0s";
-            document.getElementById("interactiveContainer").style.transitionDuration = "1s";
+            document.getElementById("octopusBody").className = "hoverOctopusBody";
+            document.getElementById("octopusMouth").className = "hoverOctopusMouth";
+            document.getElementById("octopusLeg1").className = "hoverOctopusLeg1";
+            document.getElementById("octopusLeg2").className = "hoverOctopusLeg2";
+            document.getElementById("octopusLeg3").className = "hoverOctopusLeg3";
+            interactiveContainer.style.backgroundColor = "#bb99ff";
+            interactiveContainer.style.transitionDelay = "0s";
+            interactiveContainer.style.transitionDuration = "1s";
         }, 6000);
     }
 
