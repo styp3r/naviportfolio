61d95c698cf339074cd9558a2a7cda3838ea43d6
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-04 04:23:06 +0530

    Code refactoring and debugging- after glowball appears, there was a glitch in animation (fixed).

--- src/components/GlowBall.jsx
+++ src/components/GlowBall.jsx
@@ -14,12 +14,12 @@ function GlowBall(){
         document.getElementById("octopusLeg1").className = "octopusLeg1Slurp";
         document.getElementById("octopusLeg2").className = "octopusLeg2Slurp";
         document.getElementById("octopusLeg3").className = "octopusLeg3Slurp";
-        document.getElementById("heroText1").style.color = "#bb99ff";
-        document.getElementById("heroText1").style.transitionDelay = "2.5s";
-        interactiveContainer.style.backgroundColor = "#151e1e";
-        interactiveContainer.style.transitionDelay = "2.5s";
-        interactiveContainer.style.transitionDuration = "0.7s";
-
+        
+        setTimeout(()=>{
+            glowBall.style.display = "none";
+        }, 1500);
+        
+        
         setTimeout(()=>{
             octopusEye.style.backgroundColor = "#ff9933";
             octopusEye.style.transitionDuration = "1s";
@@ -27,24 +27,22 @@ function GlowBall(){
             octopusPupil.style.backgroundColor = "red";
             octopusPupil.style.transitionDelay = "0.5s";
             octopusPupil.style.transitionDuration = "1s";
-        }, 2000);
+        }, 1500);
 
         setTimeout(()=>{
+            interactiveContainer.style.backgroundColor = "#151e1e";
+            interactiveContainer.style.transitionDuration = "0.7s";
+            document.getElementById("heroText1").style.color = "#bb99ff";
+
             octopusMouth.style.width = "1vmin";
             octopusMouth.style.height = "1vmin";
             octopusMouth.style.borderRadius = "100%";
             octopusMouth.style.transitionDelay = "0.5s";
             octopusMouth.style.transitionDuration = "1s";
-        }, 2500);
+        }, 2000);
         
 
         setTimeout(() => {
-            glowBall.className = "";
-            glowBall.style.width = "5vmin";
-            glowBall.style.height = "5vmin";
-            glowBall.style.transitionDuration = "1s";
-            document.getElementById("heroText1").style.color = "#000000";
-            document.getElementById("heroText1").style.transitionDelay = "0s";
             document.getElementById("octopusHead").className = "hoverOctopusHead";
             document.getElementById("octopusBody").className = "hoverOctopusBody";  
             octopusMouth.style.transitionDuration = "1s";
@@ -60,14 +58,24 @@ function GlowBall(){
             document.getElementById("octopusLeg1").className = "hoverOctopusLeg1";
             document.getElementById("octopusLeg2").className = "hoverOctopusLeg2";
             document.getElementById("octopusLeg3").className = "hoverOctopusLeg3";
+        }, 6000);
+
+        setTimeout(()=>{
+            document.getElementById("heroText1").style.color = "#000000";
+            document.getElementById("heroText1").style.transitionDelay = "0s";
+            glowBall.className = "";
+            glowBall.style.transitionDuration = "0s";
+            glowBall.style.display = "block";
             interactiveContainer.style.backgroundColor = "#bb99ff";
             interactiveContainer.style.transitionDelay = "0s";
-            interactiveContainer.style.transitionDuration = "1s";
-        }, 6000);
+            interactiveContainer.style.transitionDuration = "0.5s";
+        }, 8000);
     }
 
     return (
-        <div onMouseOver = {toggleDark} id = "glowBall"></div>
+        <div>
+            <div onMouseOver = {toggleDark} id = "glowBall"></div>
+        </div>
     );
 }
 
