c9f7e2779db68bf0e122bd8d61b296f200b38880
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-04 07:22:45 +0530

    Creates glowmonster upper and lower body

--- src/components/GlowMonster.jsx
+++ src/components/GlowMonster.jsx
@@ -1,5 +1,8 @@
 function GlowMonster(){
-    return(null);
+    return(<div className = "glowMonsterContainer">
+        <div id = "glowMonsterUpperBody"></div>
+        <div id = "glowMonsterLowerBody"></div>
+    </div>);
 }
 
 export default GlowMonster;
