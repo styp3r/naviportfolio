dc258d2347c566175066630b690b558587f791b7
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-07 23:57:10 +0530

    Logic for mousemove parallax

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import Intro from './components/Intro';
 
 function App() {
 
+  document.addEventListener('mousemove', Parallax);
+
+function Parallax(e){
+    let x = e.offsetX; //x-coordinate
+    let y = e.offsetY; //y-coordinate
+    console.log(x+" "+y);
+
+    document.getElementById("bigMonster").style.transform = "translate("+(-x/100)+"%, "+(-y/100)+"%)";
+    document.getElementById("bigMonsterReflection").style.transform = "translate("+(-x/100)+"%, "+(-y/100)+"%)";
+    document.getElementById("bigMonsterBody").style.transform = "translate("+(-x/100)+"%, "+(-y/100)+"%)";
+    document.getElementById("bigMonsterBodyReflection").style.transform = "translate("+(-x/100)+"%, "+(-y/100)+"%)";
+    document.getElementById("bigMonsterNeck").style.transform = "translate("+(-x/100)+"%, "+(-y/100)+"%)";
+    document.getElementById("bigMonsterChest").style.transform = "translate("+(-x/100)+"%, "+(-y/100)+"%)";
+    document.getElementById("glowMonsterUpperBody").style.transform = "translate("+(-x/100)+"%, "+(-y/100)+"%)";
+    //document.getElementById("glowMonsterLowerBody").style.transform = "translate("+(x/100)+"%, "+(y/100)+"%)";
+    document.getElementById("octopusHead").style.transform = "translate("+(-x/100)+"%, "+(-y/100)+"%)";
+
+    
+}
   return (  
     <div className = "App">
     <Hero />
