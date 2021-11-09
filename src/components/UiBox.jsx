5146ec03afb00d9b0b40ea49545804eea9b707f2
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-09 13:13:35 +0530

    White border box animation around heroContentContainer

---
+++ src/components/UiBox.jsx
@@ -0,0 +1,12 @@
+import Cross from '../images/cross.png';
+
+function UiBox(){
+    return(
+        <div id = "heroBoxContainer">
+            <div id = "heroBox"></div>
+            <img id = "heroCross" src = {Cross} width= "50" height = "50"/>
+        </div>
+    );
+}
+
+export default UiBox;
