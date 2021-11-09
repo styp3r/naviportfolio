fc2fd456fe1961a2b9127e0262821dc065426ecf
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-09 19:42:10 +0530

    Adds Blob, HeroLine  and DarkMode components

--- src/components/InteractiveContainer.jsx
+++ src/components/InteractiveContainer.jsx
@@ -1,13 +1,17 @@
 import UiBox from './UiBox';
 import Social from './SocialMediaIcons';
-import CircleExpand from './CircleExpand';
+import HeroLine from './HeroLine';
+import Blob from './Blob';
+import DarkModeToggle from './DarkModeToggle';
 
 function InteractiveContainer(){
 
-    return(<div>
+    return(<div id = "interactiveContainer">
             <UiBox />
             <Social />
-            <CircleExpand />
+            <HeroLine />
+            <Blob />
+            <DarkModeToggle />
             </div>
         );
 }
