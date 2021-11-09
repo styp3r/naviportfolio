74361121288bc57bc554fb6f8771745943d38568
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-09 13:12:49 +0530

    Social media icon buttons from fontawesome

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,11 @@
+function SocialMediaIcons(){
+    return (
+            <div id = "socialMediaIconsContainer">
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-github"></i>
+                <i class="fab fa-linkedin"></i>
+            </div>);
+}
+
+export default SocialMediaIcons;
