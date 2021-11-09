b611c182fdc797c92b5486dda1da9aca306178d7
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-09 13:11:34 +0530

    Single component to hold all heroContainer animation elements

---
+++ src/components/InteractiveContainer.jsx
@@ -0,0 +1,12 @@
+import UiBox from './UiBox';
+import Social from './SocialMediaIcons';
+
+function InteractiveContainer(){
+    return(<div>
+            <UiBox />
+            <Social />
+            </div>
+        );
+}
+
+export default InteractiveContainer;
