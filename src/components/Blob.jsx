30b3a741b881b98b44130e3a88ebcfd3e03f35a4
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-09 19:43:17 +0530

    Creates 3 Blob circles in interactiveContainer with blur filter

---
+++ src/components/Blob.jsx
@@ -0,0 +1,10 @@
+function Blob(){
+    return(<div>
+            <div id = "blob1"></div>
+            <div id = "blob2"></div>
+            <div id = "blob3"></div>
+        </div>
+    );
+}
+
+export default Blob;
