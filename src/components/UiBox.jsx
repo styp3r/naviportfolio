b0fa55b53e1d33ab19d944960a074b5eb12d92c1
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-11 16:32:09 +0530

    Adds another cross of the same image for socialMediaContainer animation

--- src/components/UiBox.jsx
+++ src/components/UiBox.jsx
@@ -5,6 +5,7 @@ function UiBox(){
         <div id = "heroBoxContainer">
             <div id = "heroBox"></div>
             <img id = "heroCross" src = {Cross} width= "50" height = "50"/>
+            <img id = "heroCross1" src = {Cross} width= "50" height = "50"/>
         </div>
     );
 }
