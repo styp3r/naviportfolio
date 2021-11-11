cdfba75ae76561990843b83e559f62e2c147e0f2
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-11 16:34:59 +0530

    Mouse Scroll animation at the bottom of the screen to indicate the user to scroll down.

---
+++ src/components/MouseScroll.jsx
@@ -0,0 +1,11 @@
+function MouseScroll(){
+    return (
+        <div id = "mouseScrollIcon">
+            <div id = "mouse">
+                <div id = "scrollWheel"></div>
+            </div>
+        </div>
+    );
+}
+
+export default MouseScroll;
