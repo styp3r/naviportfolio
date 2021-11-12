db215191232b47812c939308205776f4e7331651
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-13 01:44:38 +0530

    Collection of projectItem components to show all the projects i've done

---
+++ src/components/Work.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProjectItem from './ProjectItem';
+
+function Work(){
+    return (<div id = "workContainer">
+        <h1 id = "workTitle">Some things I've worked on</h1>
+        <ProjectItem title = "one" itemName = "Project .001"/>
+        <ProjectItem title = "two" itemName = "Project .002"/>
+        <ProjectItem title = "three" itemName = "Project .003"/>
+    </div>);
+}
+
+export default Work;
