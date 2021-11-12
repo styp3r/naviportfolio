cdce99099980844d91a6b97f2f4a3bdd9a5b5c89
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-12 23:36:39 +0530

    Comments out parallax function and adds console.info message

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Hero from './components/Hero';
 import Intro from './components/Intro';
 
 function App() {
-
+/*
   document.addEventListener('mousemove', Parallax);
 
 function Parallax(e){
     let x = e.offsetX; //x-coordinate
     let y = e.offsetY; //y-coordinate
     console.log(x+" "+y);
-}
+} */
+
+  console.info("WEEWOO 🚨 WEEWOO 🚨 NAVJYOTJAISWAL.COM POLICE HERE 🚓👮‍♂️ JUST CHECKING 👀 IF EVERYTHING IS SAFE 🤸‍♂️");
   return (  
     <div className = "App">
     <Hero />
