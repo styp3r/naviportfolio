f0e7401f96a60bd0946672d356b0b45db14ee206
Navjyot Jaiswal <stypertypes@gmail.com>
2021-11-15 01:37:07 +0530

    Code Refactor

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import ProjectItem from './ProjectItem';
 function Work(){
 
     return (<div id = "workContainer">
-        <h1 id = "workTitle">Some things I've worked on</h1>
+        <h1 id = "workTitle">Work</h1>
         <ProjectItem title = "one" itemName = "Project .001"/>
         <ProjectItem title = "two" itemName = "Project .002"/>
         <ProjectItem title = "three" itemName = "Project .003"/>
