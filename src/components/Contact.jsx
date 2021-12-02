041552d1fab03c48a4d689a22f4fa039f84dee11
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-02 17:04:52 +0530

    New component to display contact details

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact(){
+    return (
+        <div id = "contactContainer">
+            <h1 className = "contactHeading">Let's talk!</h1>
+            <div></div>
+        </div>
+    );
+}
+
+export default Contact;
