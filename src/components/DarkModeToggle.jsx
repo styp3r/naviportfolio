ffacf9b8f7f3160c7a0b21969a81ff7e024e7345
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-10 05:40:30 +0530

    Changes button color combination on toggle

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -4,18 +4,6 @@ function DarkModeToggle(){
 
     const [dark, setDark] = useState(0);
 
-    function handleHoverOn(){
-        document.getElementById("darkModeToggle").style.border = "solid #bf80ff";
-        document.getElementById("darkModeToggle").style.borderWidth = "0.5vmin";
-        document.getElementById("darkModeToggle").style.transitionDuration = "0.5s";
-    }
-
-    function handleHoverOut(){
-        document.getElementById("darkModeToggle").style.border = "solid aqua";
-        document.getElementById("darkModeToggle").style.borderWidth = "0.5vmin";
-        document.getElementById("darkModeToggle").style.transitionDuration = "3s";
-    }
-
     function changeSq(){
         document.getElementById("sq").style.backgroundColor = "#bf80ff";
         document.getElementById("sq").style.transitionDuration = "0.2s";
@@ -29,22 +17,28 @@ function DarkModeToggle(){
 
     function toggleDarkMode(){
         if(dark === 0){
+            document.getElementById("darkModeToggle").style.backgroundColor = "#262626";
+            document.getElementById("darkModeToggle").style.border = "solid #ffffff";
+            document.getElementById("sq").style.backgroundColor = "#00e5ff";
             document.getElementById("workTitle").style.color = "#ffffff";
             document.getElementById("heroText1").style.color = "#aa80ff";
             document.getElementById("heroContainer").style.background = "linear-gradient(45deg, #262626 , #262626)";
-            document.getElementById("workContainer").style.backgroundColor = "#262626";
+            document.getElementById("workContainer").style.background = "linear-gradient(45deg, #262626 , #262626)";
             setDark(1);
         } else {
+            document.getElementById("darkModeToggle").style.border = "solid #00e5ff";
+            document.getElementById("sq").style.backgroundColor = "#000000";
+            document.getElementById("darkModeToggle").style.backgroundColor = "#ffffff";
             document.getElementById("workTitle").style.color = "#000000";
             document.getElementById("heroText1").style.color = "#9933ff";
-            document.getElementById("workContainer").style.backgroundColor = "#ffffff";
-            document.getElementById("heroContainer").style.background = "linear-gradient(20deg, #facd68, #fc76b3)";
+            document.getElementById("workContainer").style.background = "linear-gradient(135deg, #00e5ff, #ffffff)";
+            document.getElementById("heroContainer").style.background = "linear-gradient(45deg, #00e5ff, #ffffff)";
             setDark(0);
         }
     }
 
     return (
-        <div onMouseOver = {handleHoverOn} onMouseOut = {handleHoverOut} onMouseDown = {changeSq} onMouseUp = {changeSqToNormal} onClick = {toggleDarkMode} id = "darkModeToggle">
+        <div onMouseDown = {changeSq} onMouseUp = {changeSqToNormal} onClick = {toggleDarkMode} id = "darkModeToggle">
             <div id = "sq"></div>
         </div>
     );
