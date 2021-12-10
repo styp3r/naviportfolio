379e544b39d0810aa0de9a9973a5ff951f606737
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-10 06:34:20 +0530

    Removes big circle component as the design does not require it.

--- src/components/BigCircle.jsx
+++
@@ -1,7 +0,0 @@
-import bigCircle from '../images/bigCircle.png';
-
-function BigCircle(){
-    return <img id = "bigCircle" src = {bigCircle} alt = "A big rotating circle graphic"></img>;
-}
-
-export default BigCircle;
