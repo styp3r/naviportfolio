a14b61fc9e01fff03ff77c6e3920a316b83687ba
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-10 07:26:12 +0530

    Adds social media links to profiles that opens in a new tab

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,10 +1,10 @@
 function SocialMediaIcons(){
     return (
             <div id = "socialMediaIconsContainer">
-                <i className ="fab fa-instagram"></i>
-                <i className ="fab fa-twitter"></i>
-                <i className ="fab fa-github"></i>
-                <i className ="fab fa-linkedin"></i>
+                <a className = "socialLink" href = "https://www.instagram.com/5typ3r/" target="_blank"><i className ="fab fa-instagram"></i></a>
+                <a className = "socialLink" href = "https://twitter.com/styp3r" target="_blank"><i className ="fab fa-twitter"></i></a>
+                <a className = "socialLink" href = "https://github.com/styp3r" target="_blank"><i className ="fab fa-github"></i></a>
+                <a className = "socialLink" href = "https://www.linkedin.com/in/navjyotjaiswal/" target="_blank"><i className ="fab fa-linkedin"></i></a>
             </div>);
 }
 
