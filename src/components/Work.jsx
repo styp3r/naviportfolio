c457abbcfb9211c1165a59c2c295a4cf1537c828
Navjyot Jaiswal <stypertypes@gmail.com>
2021-12-10 06:36:57 +0530

    Adds curly braces on either sides of work title

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,8 +3,11 @@ import ProjectItem from './ProjectItem';
 
 function Work(){
 
+    let curlyLeft = "{";
+    let curlyRight = "}";
+
     return (<div id = "workContainer">
-        <h1 id = "workTitle">Work</h1>
+        <h1 id = "workTitle">{curlyLeft} Some of my projects {curlyRight}</h1>
         <ProjectItem title = "one" itemName = "Project .001"/>
         <ProjectItem title = "two" itemName = "Project .002"/>
         <ProjectItem title = "three" itemName = "Project .003"/>
